71c6a02563e5383954497b150923072b44246464
David Lumaye <chindit@users.noreply.github.com>
2020-03-20 09:15:07 +0100

    Remove $DIR_TO_SCAN

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,8 @@ set -e
 
 PHP_FULL_VERSION=$(php -r 'echo phpversion();')
 
-if [ -z "$1" ]; then
-  DIR_TO_SCAN="."
-else
-  DIR_TO_SCAN="$1"
-fi
 
-echo "## Running PHPCS on ${DIR_TO_SCAN}"
+echo "## Running PHPCS"
 echo "PHP Version : ${PHP_FULL_VERSION}"
 
-if [ ! -d "${DIR_TO_SCAN}" ] && [ ! -f "${DIR_TO_SCAN}" ]; then
-  echo "\nInvalid directory or file: ${DIR_TO_SCAN}"
-  echo "\n\n"
-
-  exit 2
-fi
-
-php -d memory_limit=-1 /phpcs -v ${DIR_TO_SCAN}
+php -d memory_limit=-1 /phpcs -v
