ccda708ed581ebe13a66497cc47c07b8346af3b3
David Lumaye <chindit@users.noreply.github.com>
2020-03-20 09:17:22 +0100

    Add progress

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ PHP_FULL_VERSION=$(php -r 'echo phpversion();')
 echo "## Running PHPCS"
 echo "PHP Version : ${PHP_FULL_VERSION}"
 
-php -d memory_limit=-1 /phpcs -v
+php -d memory_limit=-1 /phpcs -v -p
